164ad39afe9bf965b7bc8720fb683986846bc854
Usama Rashid Butt <usama.rashid@devsinc.com>
2023-03-03 13:42:27 +0500

    BackUp commit

---
+++ tutorial/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // println!("Hello, world!");
+    let xx = 4;
+    let x = 4;
+    let y = 2;
+    let mut z = 0;
+    z = x + y;
+    let y = 5;
+    println!("{}", y);
+    println!("{}", z);
+    println!("{}", xx);
+}
